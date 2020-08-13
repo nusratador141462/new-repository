85e92036d1ce20a47ce2d74d0e7c2ad87b2bf221
nusrat ador <nusratador141462@gmail.com>
2020-08-13 08:57:29 -0700

    all method remove and add message method

--- hello1.java
+++ hello1.java
@@ -1,15 +1,14 @@
 public class hello{
 	public static void public static void main(String[] args) {
 		System.out.printIn("Vscode is Awesome...");
-
-			greeting("NS Ador");
-	}
-}
-
-public static void greeting(String name){
-  System.out.printIn("welcome" + name);
-  
-  public static int sqr(int x) {
-    return x*x;
   }
-}
+
+
+		public static void message (){
+      System.out.printIn("Hurrah!Now we are able to contribute on open source...")
+    }
+
+    
+	}
+
+
