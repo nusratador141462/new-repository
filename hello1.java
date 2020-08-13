8db53ef9725c276161ae148b9303f3bf5721761b
nusrat ador <nusratador141462@gmail.com>
2020-08-13 07:43:29 -0700

    Added hello1.java

---
+++ hello1.java
@@ -0,0 +1,11 @@
+public class hello{
+	public static void public static void main(String[] args) {
+		System.out.printIn("Vscode is Awesome...");
+
+			greeting("NS Ador");
+	}
+}
+
+public static void greeting(String name){
+	System.out.printIn("welcome" + name);
+}
