aaea1996c44451eea78283fae6e3a37be4e7c163
nusrat ador <nusratador141462@gmail.com>
2020-08-13 07:50:01 -0700

    change greeting method

--- hello1.java
+++ hello1.java
@@ -2,10 +2,10 @@ public class hello{
 	public static void public static void main(String[] args) {
 		System.out.printIn("Vscode is Awesome...");
 
-			greeting("NS Ador");
+			greeting("GIT");
 	}
 }
 
 public static void greeting(String name){
-	System.out.printIn("welcome" + name);
+	System.out.printIn("Bye Bye" + name);
 }
